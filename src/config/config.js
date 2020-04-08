0ebc8f73890f8542db348c31c678b600a898fe61
shagunchaurasia <chaurasia.shagun@gmail.com>
2020-04-08 14:47:34 +0900

    Added dotenv configurations to follow 12 factor steps

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv').config();
+
+//JSON.parse added specifically for escaping \\n character 
+module.exports ={
+    googleProjectId                 : process.env[process.env.ENV+"_GOOGLE_PROJECT_ID"],
+    dialogFlowSessionId             : process.env[process.env.ENV+"_DIALOG_FLOW_SESSION_ID"],
+    dialogFlowSessionLanguageCode   : process.env[process.env.ENV+"_DIALOG_FLOW_SESSION_LANGUAGE_CODE"],
+    googleClientEmail               : process.env[process.env.ENV+"_GOOGLE_CLIENT_EMAIL"],
+    googlePrivateKey               : JSON.parse(`"${process.env[process.env.ENV+"_GOOGLE_PRIVATE_KEY"]}"`),
+
+}
+
--- src/controllers/dialogFlowController.js
+++ src/controllers/dialogFlowController.js
@@ -1,18 +1,35 @@
 const dialogFlow = require('dialogflow');
-const config     = require('./../config/keys');
-const sessionClient = new dialogFlow.SessionsClient();
-const sessionPath = sessionClient.sessionPath(config.googleProjectId, config.dialogFlowSessionId);
+const config     = require('./../config/config');
+const {struct} = require('pb-util');
+
+const googleProjectId = config.googleProjectId;
+const credentials = {
+    client_email : config.googleClientEmail,
+    private_key  : config.googlePrivateKey
+}
+
+console.log(credentials);
+
+
+const sessionClient = new dialogFlow.SessionsClient({projectId:googleProjectId,credentials: credentials});
+
+const sessionPath = sessionClient.sessionPath(googleProjectId, config.dialogFlowSessionId);
 
 exports.dfTextQuery =  async (request,response,next)=>{
 
     console.log(request.body)
-
+    
     const requestToSend = {
         session         : sessionPath,
         queryInput      : {
             text        : {
                 text            : request.body.text,
                 languageCode    : config.dialogFlowSessionLanguageCode
+            }
+        },
+        queryParams     : {
+            payload     : {
+                data    : request.body.params
             }
         }
     }
@@ -25,10 +42,31 @@ exports.dfTextQuery =  async (request,response,next)=>{
 }
 
 
-exports.dfEventQuery = (request,response,next)=>{
+exports.dfEventQuery = ( async (request,response,next)=>{
 
-    response.json({'do':'df_event_query'})
+    console.log(request.body)
+    
+    const requestToSend = {
+        session         : sessionPath,
+        queryInput      : {
+            event        : {
+                name            : request.body.event,
+                parameters      : struct.encode(parameters),
+                languageCode    : config.dialogFlowSessionLanguageCode
+            }
+        },
+        queryParams     : {
+            payload     : {
+                data    : request.body.params
+            }
+        }
+    }
 
-}
+    let responses = await sessionClient.detectIntent(requestToSend)
+    response.send(responses[0].queryResult);
+    
+  
+
+})
 
 
